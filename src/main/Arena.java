d96068692d60cc90bff666fbb1b51ecc61626bb5
H-A-L-F <arya.jayadi2013@gmail.com>
2024-05-07 11:18:15 +0700

    Arena blueprint

--- src/main/Arena.java
+++ src/main/Arena.java
@@ -1,7 +1,43 @@
 package main;
 
-public class Arena {
-    public void play() {
+import models.Pet;
 
+import java.util.Vector;
+
+public class Arena {
+    private Vector<Pet> pets, enemy;
+    int round, win, life;
+
+    public Arena() {
+        pets = new Vector<>(5);
+        enemy = new Vector<>(5);
+        round = 0;
+        win = 0;
+        life = 5;
+    }
+
+    public void play() {
+        while (life > 0) {
+            round++;
+            shop();
+            int res = battle();
+            if (res == 0) {
+                // draw
+            } else if (res == -1) {
+                // lose
+                life--;
+            } else if (res == 1) {
+                // win
+                win++;
+            }
+        }
+    }
+
+    public void shop() {
+
+    }
+
+    public int battle() {
+        return 0;
     }
 }
--- src/models/Pet.java
+++ src/models/Pet.java
@@ -1,12 +1,14 @@
 package models;
 
+import constants.PetStatus;
 import interfaces.Ability;
 
-public class Pet {
+public abstract class Pet {
     private String name;
     private int atk;
     private int hp;
     private Ability ability;
+    private PetStatus status;
 
     public Pet(String name, int atk, int hp, Ability ability) {
         this.name = name;
@@ -14,4 +16,6 @@ public class Pet {
         this.hp = hp;
         this.ability = ability;
     }
+
+    public abstract void move();
 }
