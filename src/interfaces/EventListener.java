03341c2ea6855f65e1977909899d95cc2362c79a
H-A-L-F <arya.jayadi2013@gmail.com>
2024-05-08 11:44:04 +0700

    eventlistener

---
+++ src/interfaces/EventListener.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface EventListener {
+    public abstract void update();
+}
---
+++ src/main/EventManager.java
@@ -0,0 +1,33 @@
+package main;
+
+import constants.Event;
+import interfaces.EventListener;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class EventManager {
+    private HashMap<Event, EventListener>listeners;
+    private static EventManager instance;
+
+    private EventManager() {
+        listeners = new HashMap<>();
+    }
+
+    public static EventManager getInstance() {
+        if (instance == null) instance = new EventManager();
+        return instance;
+    }
+
+    public void subscribe(EventListener listener, Event event) {
+        listeners.put(event, listener);
+    }
+
+    public void unsubscribe(EventListener listener, Event event) {
+        listeners.remove(event, listener);
+    }
+
+    public void notify(Event event) {
+        listeners.get(event).update();
+    }
+}
