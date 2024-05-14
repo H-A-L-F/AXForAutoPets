bce774a954378a4b374074eceade7a4e5d0fc403
H-A-L-F <arya.jayadi2013@gmail.com>
2024-05-15 02:16:09 +0700

    refactor now using method

--- src/constants/PetFactory.java
+++ src/constants/PetFactory.java
@@ -7,35 +7,32 @@ import models.Team;
 public class PetFactory {
     private Team team;
 
-    // TIER 1
-    public Pet ant;
-    public Pet pig;
-    public Pet fish;
-    public Pet cricket;
-    public Pet zombieCricket;
-
-    // TIER 2
-
     public PetFactory(Team team) {
         this.team = team;
+    }
 
-        this.ant = new Pet(PetList.ANT, 1, 2, 2) {
+    public Pet getAnt() {
+        return new Pet(PetList.ANT, 1, 2, 2) {
             @Override
             public void onFaint(int pos) {
                 super.onFaint(pos);
                 team.getRandPet().buff(getLv(), getLv());
             }
         };
+    }
 
-        this.pig = new Pet(PetList.PIG, 1, 4, 1) {
+    public Pet getPig() {
+        return new Pet(PetList.PIG, 1, 4, 1) {
             @Override
             public void onSell() {
                 super.onSell();
                 Arena.getInstance().incMoney(getLv());
             }
         };
+    }
 
-        this.fish = new Pet(PetList.FISH, 1, 2, 3) {
+    public Pet getFish() {
+        return new Pet(PetList.FISH, 1, 2, 3) {
             @Override
             public void onLvUp() {
                 super.onLvUp();
@@ -44,18 +41,22 @@ public class PetFactory {
                 }
             }
         };
+    }
 
-        this.cricket = new Pet(PetList.CRICKET, 1, 1, 2) {
+    public Pet getCricket() {
+        return new Pet(PetList.CRICKET, 1, 1, 2) {
             @Override
             public void onFaint(int pos) {
                 super.onFaint(pos);
-                Pet temp = zombieCricket;
+                Pet temp = getZombieCricket();
                 temp.setStats(getLv(), getLv());
                 team.addPet(temp, pos);
             }
         };
+    }
 
-        this.zombieCricket = new Pet(PetList.ZOMBIE_CRICKET, 1, 1, 1) {
+    public Pet getZombieCricket() {
+        return new Pet(PetList.ZOMBIE_CRICKET, 1, 1, 1) {
         };
     }
 }
