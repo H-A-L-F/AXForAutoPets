094ec02c250b4620185afdf117a58cd043288b9d
H-A-L-F <arya.jayadi2013@gmail.com>
2024-05-17 08:45:28 +0700

    addpet system

--- src/models/Team.java
+++ src/models/Team.java
@@ -9,10 +9,12 @@ public class Team {
     private EventManager eventManager;
     private Vector<Pet> pets;
     private final int SIZE = 5;
+    private int slot;
 
     public Team() {
         this.eventManager = new EventManager(this);
         pets = new Vector<>(SIZE);
+        slot = SIZE;
     }
 
     public void printTeam() {
@@ -39,6 +41,21 @@ public class Team {
     }
 
     public void addPet(Pet pet, int pos) {
+        if(slot == 0) {
+            failSpawn(pet);
+            return;
+        }
+        if(pets.get(pos) == null) {
+            pets.insertElementAt(pet, pos);
+            return;
+        }
+        for(int i = SIZE - 1; i > pos; i--) {
+            pets.insertElementAt(pets.get(i - 1), i);
+        }
         pets.insertElementAt(pet, pos);
+    }
+
+    private void failSpawn(Pet pet) {
+        System.out.println("Failed to spawn pet");
     }
 }
