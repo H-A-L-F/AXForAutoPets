dae2f4cd9f4c9806afbc0688b11e4ebbfba2b4d3
H-A-L-F <arya.jayadi2013@gmail.com>
2024-05-30 10:26:26 +0700

    now prints exp and lv on shops

--- src/constants/Lib.java
+++ src/constants/Lib.java
@@ -31,49 +31,56 @@ public class Lib {
     public static void printSlots(Pet[] pets) {
         StringBuilder firstLn = new StringBuilder();
         StringBuilder secondLn = new StringBuilder();
-        for (Pet pet : pets) petPrintWrapper(firstLn, secondLn, pet);
+        StringBuilder thirdLn = new StringBuilder();
+        for (Pet pet : pets) petPrintWrapper(firstLn, secondLn, thirdLn, pet);
         System.out.println(firstLn);
         System.out.println(secondLn);
+        System.out.println(thirdLn);
     }
 
-    private static void printEmpty(StringBuilder firstLn, StringBuilder secondLn) {
+    private static void printEmpty(StringBuilder firstLn, StringBuilder secondLn, StringBuilder thirdLn) {
         firstLn.append(String.format("[ %5s ] ", ""));
         secondLn.append(String.format("[ %d | %d ] ", 0, 0));
+        thirdLn.append(String.format("[ %d:%d/%d ] ", 0, 0, 0));
     }
 
     static PetPrinter petPrinter = new PetPrinter() {
         @Override
-        public void print(StringBuilder firstLn, StringBuilder secondLn, Pet pet) {
+        public void print(StringBuilder firstLn, StringBuilder secondLn, StringBuilder thirdLn, Pet pet) {
             String first = String.format("[ %-5s ]", center(pet.getName(), 5));
             String second = String.format("%d | %d", pet.getAtk(), pet.getHp());
+            String third = String.format("%d:%d/%d", pet.getLv(), pet.getExp(), pet.getExpLimit());
             int len = first.length() - 4;
             String secondFormat = "[ %-" + len + "s ]";
             firstLn.append(first).append(" ");
             secondLn.append(String.format(secondFormat, center(second, len))).append(" ");
+            thirdLn.append(String.format(secondFormat, center(third, len))).append(" ");
         }
     };
 
-    private static void petPrintWrapper(StringBuilder firstLn, StringBuilder secondLn, Pet pet) {
-        if(pet == null) printEmpty(firstLn, secondLn);
-        else petPrinter.print(firstLn, secondLn, pet);
+    private static void petPrintWrapper(StringBuilder firstLn, StringBuilder secondLn, StringBuilder thirdLn, Pet pet) {
+        if (pet == null) printEmpty(firstLn, secondLn, thirdLn);
+        else petPrinter.print(firstLn, secondLn, thirdLn, pet);
     }
 
     public static void printTeams(Team pteam, Team eTeam) {
         StringBuilder firstLn = new StringBuilder();
         StringBuilder secondLn = new StringBuilder();
-        for(int i = 0; i < Team.END_SIZE; i++) {
+        StringBuilder thirdLn = new StringBuilder();
+        for (int i = 0; i < Team.END_SIZE; i++) {
             Pet pet = pteam.getPet(i);
-            petPrintWrapper(firstLn, secondLn, pet);
+            petPrintWrapper(firstLn, secondLn, thirdLn, pet);
         }
         String vs = "-> <- ";
         firstLn.append(vs);
         secondLn.append(String.format("%" + vs.length() + "s", ""));
-        for(int i = Team.END_SIZE; i >= 0; i--) {
+        for (int i = Team.END_SIZE; i >= 0; i--) {
             Pet pet = eTeam.getPet(i);
-            petPrintWrapper(firstLn, secondLn, pet);
+            petPrintWrapper(firstLn, secondLn, thirdLn, pet);
         }
         System.out.println(firstLn);
         System.out.println(secondLn);
+//        System.out.println(thirdLn);
     }
 
     public static void printDivider() {
@@ -97,5 +104,5 @@ public class Lib {
 }
 
 interface PetPrinter {
-    public abstract void print(StringBuilder firstLn, StringBuilder secondLn, Pet pet);
+    public abstract void print(StringBuilder firstLn, StringBuilder secondLn, StringBuilder thirdLn, Pet pet);
 }
