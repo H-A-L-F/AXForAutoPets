8147145cf840fd1811c5b3cc07385b8cd3f33360
H-A-L-F <arya.jayadi2013@gmail.com>
2024-06-26 16:53:14 +0700

    Update DevelopmentState.java

--- src/constants/DevelopmentState.java
+++ src/constants/DevelopmentState.java
@@ -10,7 +10,7 @@ public enum DevelopmentState {
 
     public static final ArrayList<DevelopmentState> state = new ArrayList<>(Arrays.asList(
             INSERT_MATCH,
-//            GET_ENM_ROUND,
+            GET_ENM_ROUND,
             INSERT_ROUND
     ));
 }
